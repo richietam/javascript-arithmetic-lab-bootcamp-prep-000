c8caec3018430d0787781104c1d89c43c0c72b46
richardtam.ny <richardtam.ny@gmail.com>
2018-11-21 22:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function add(a, b) {
 function subtract(a, b) {
   return (a -= b)
 }
+
+function multiply(a, b) {
+  return (a *= b)
+}
+
+function divide(a, b) {
+  return (a /= b)
+}
+
+var n = 1
+
+function inc(n) {
+  return (n++)
+}
